a19c645d6249edcdae783dbb0fffff3e4274f4ab
Ronak <ronakcode11@gmail.com>
2022-09-05 18:10:25 +0530

    maincommit

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get("/work",function(req,res){
 app.get("/about",function(req,res){
     res.render("about")
 })
-app.listen(3000, function () {
+app.listen(process.env.PORT  || 3000, function () {
     console.log("Server is running on port 3000");
 })
 
